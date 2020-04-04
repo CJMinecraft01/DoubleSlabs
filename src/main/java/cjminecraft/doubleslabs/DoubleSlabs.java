bc81be0e622cad5a0aaf7c59e927d166d893ce7f
CJMinecraft01 <speed2pc2@gmail.com>
2020-04-04 15:29:24 +0100

    Update 1.1.0

--- src/main/java/cjminecraft/doubleslabs/DoubleSlabs.java
+++ src/main/java/cjminecraft/doubleslabs/DoubleSlabs.java
@@ -10,11 +10,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(DoubleSlabs.MODID)
 public class DoubleSlabs
 {
     public static final String MODID = "doubleslabs";
+    public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
     public static IProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
--- src/main/java/cjminecraft/doubleslabs/Events.java
+++ src/main/java/cjminecraft/doubleslabs/Events.java
@@ -1,5 +1,7 @@
 package cjminecraft.doubleslabs;
 
+import cjminecraft.doubleslabs.api.ISlabSupport;
+import cjminecraft.doubleslabs.api.SlabSupport;
 import cjminecraft.doubleslabs.tileentitiy.TileEntityDoubleSlab;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.BlockState;
@@ -22,45 +24,56 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = DoubleSlabs.MODID)
 public class Events {
 
-    private static void tryPlace(BlockState state, SlabType half, BlockPos pos, SlabBlock slab, PlayerInteractEvent.RightClickBlock event) {
-        BlockState slabState = slab.getDefaultState().with(SlabBlock.TYPE, half == SlabType.BOTTOM ? SlabType.TOP : SlabType.BOTTOM);
-        BlockState newState = Registrar.DOUBLE_SLAB.getDefaultState();
-
-        if (Config.SLAB_BLACKLIST.get().contains(Config.slabToString(state)) || Config.SLAB_BLACKLIST.get().contains(Config.slabToString(slabState)))
-            return;
-
-        if (!event.getWorld().checkBlockCollision(event.getPlayer().getBoundingBox()) && event.getWorld().setBlockState(pos, newState, 11)) {
-            TileEntityDoubleSlab tile = (TileEntityDoubleSlab) event.getWorld().getTileEntity(pos);
-            if (tile == null)
-                return;
-            tile.setTopState(half == SlabType.TOP ? state : slabState);
-            tile.setBottomState(half == SlabType.BOTTOM ? state : slabState);
-            SoundType soundtype = slab.getSoundType(slabState, event.getWorld(), pos, event.getPlayer());
-            event.getWorld().playSound(event.getPlayer(), pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-            if (!event.getPlayer().isCreative())
-                event.getItemStack().shrink(1);
-            event.setCancellationResult(ActionResultType.SUCCESS);
-            event.setCanceled(true);
-            if (event.getPlayer() instanceof ServerPlayerEntity)
-                CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity) event.getPlayer(), pos, event.getItemStack());
-        }
-    }
-
     @SubscribeEvent
     public static void onItemUse(PlayerInteractEvent.RightClickBlock event) {
-        if (!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof BlockItem && ((BlockItem) event.getItemStack().getItem()).getBlock() instanceof SlabBlock) {
+        if (!event.getItemStack().isEmpty()) {
+            ISlabSupport itemSupport = SlabSupport.getSupport(event.getItemStack(), event.getPlayer(), event.getHand());
+            if (itemSupport == null)
+                return;
+
             if (event.getPlayer().canPlayerEdit(event.getPos().offset(event.getFace()), event.getFace(), event.getItemStack())) {
-                SlabBlock slab = (SlabBlock) ((BlockItem) event.getItemStack().getItem()).getBlock();
                 BlockPos pos = event.getPos();
                 Direction face = event.getFace();
-                if (!(event.getWorld().getBlockState(pos).getBlock() instanceof SlabBlock)) {
-                    pos = pos.offset(face);
-                    if (event.getWorld().getBlockState(pos).getBlock() instanceof SlabBlock)
-                        face = event.getWorld().getBlockState(pos).get(SlabBlock.TYPE) == SlabType.BOTTOM ? Direction.UP : Direction.DOWN;
-                }
                 BlockState state = event.getWorld().getBlockState(pos);
-                if (state.getBlock() instanceof SlabBlock && ((face == Direction.UP && state.get(SlabBlock.TYPE) == SlabType.BOTTOM) || (face == Direction.DOWN && state.get(SlabBlock.TYPE) == SlabType.TOP)) && state.get(SlabBlock.TYPE) != SlabType.DOUBLE)
-                    tryPlace(state, state.get(SlabBlock.TYPE), pos, slab, event);
+                ISlabSupport blockSupport = SlabSupport.getSupport(event.getWorld(), pos, state);
+                if (blockSupport == null) {
+                    pos = pos.offset(face);
+                    state = event.getWorld().getBlockState(pos);
+                    if (!event.getPlayer().canPlayerEdit(pos.offset(face), face, event.getItemStack()))
+                        return;
+                    blockSupport = SlabSupport.getSupport(event.getWorld(), pos, state);
+                    if (blockSupport == null)
+                        return;
+                    face = blockSupport.getHalf(event.getWorld(), pos, state) == SlabType.BOTTOM ? Direction.UP : Direction.DOWN;
+                }
+
+                SlabType half = blockSupport.getHalf(event.getWorld(), pos, state);
+                if (half == SlabType.DOUBLE)
+                    return;
+
+                if ((face == Direction.UP && half == SlabType.BOTTOM) || (face == Direction.DOWN && half == SlabType.TOP)) {
+                    BlockState slabState = itemSupport.getStateForHalf(event.getWorld(), pos, event.getItemStack(), half == SlabType.BOTTOM ? SlabType.TOP : SlabType.BOTTOM);
+                    BlockState newState = Registrar.DOUBLE_SLAB.getDefaultState();
+
+                    if (Config.SLAB_BLACKLIST.get().contains(Config.slabToString(state)) || Config.SLAB_BLACKLIST.get().contains(Config.slabToString(slabState)))
+                        return;
+
+                    if (!event.getWorld().checkBlockCollision(event.getPlayer().getBoundingBox().offset(pos)) && event.getWorld().setBlockState(pos, newState, 11)) {
+                        TileEntityDoubleSlab tile = (TileEntityDoubleSlab) event.getWorld().getTileEntity(pos);
+                        if (tile == null)
+                            return;
+                        tile.setTopState(half == SlabType.TOP ? state : slabState);
+                        tile.setBottomState(half == SlabType.BOTTOM ? state : slabState);
+                        SoundType soundtype = slabState.getSoundType(event.getWorld(), pos, event.getPlayer());
+                        event.getWorld().playSound(event.getPlayer(), pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+                        if (!event.getPlayer().isCreative())
+                            event.getItemStack().shrink(1);
+                        event.setCancellationResult(ActionResultType.SUCCESS);
+                        event.setCanceled(true);
+                        if (event.getPlayer() instanceof ServerPlayerEntity)
+                            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity) event.getPlayer(), pos, event.getItemStack());
+                    }
+                }
             }
         }
     }
