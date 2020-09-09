3b46c2c692cac3a0fe3314b44d45374bdcf6fdcd
CJMinecraft01 <speed2pc2@gmail.com>
2020-09-09 14:39:25 +0100

    Fix rotated double slab models when OptiFine is installed

--- src/main/java/cjminecraft/doubleslabs/api/Flags.java
+++ src/main/java/cjminecraft/doubleslabs/api/Flags.java
@@ -8,30 +8,31 @@ import net.minecraft.world.IBlockReader;
 
 import java.util.HashMap;
 
+@Deprecated
 public class Flags {
 
-//    private static final HashMap<BlockPos, Boolean> POSITIVE = new HashMap<>();
-//
-//    public static boolean isPositive(BlockPos pos) {
-//        return POSITIVE.getOrDefault(pos, false);
-//    }
-//
-//    public static TileEntity getTileEntityAtPos(BlockPos pos, IBlockReader world) {
-//        TileEntity tile = world.getTileEntity(pos);
+    private static final HashMap<BlockPos, Boolean> POSITIVE = new HashMap<>();
+
+    public static boolean isPositive(BlockPos pos) {
+        return POSITIVE.getOrDefault(pos, false);
+    }
+
+    public static TileEntity getTileEntityAtPos(BlockPos pos, IBlockReader world) {
+        TileEntity tile = world.getTileEntity(pos);
 //        if (tile instanceof SlabTileEntity)
 //            return isPositive(pos) ? ((SlabTileEntity) tile).getPositiveBlockInfo().getTileEntity() : ((SlabTileEntity) tile).getNegativeBlockInfo().getTileEntity();
-//        return tile;
-//    }
-//
-//    public static BlockState getBlockStateAtPos(BlockPos pos, IBlockReader world) {
-//        TileEntity tile = world.getTileEntity(pos);
+        return tile;
+    }
+
+    public static BlockState getBlockStateAtPos(BlockPos pos, IBlockReader world) {
+        TileEntity tile = world.getTileEntity(pos);
 //        if (tile instanceof SlabTileEntity)
 //            return isPositive(pos) ? ((SlabTileEntity) tile).getPositiveBlockInfo().getBlockState() : ((SlabTileEntity) tile).getNegativeBlockInfo().getBlockState();
-//        return world.getBlockState(pos);
-//    }
-//
-//    public static void setPositive(BlockPos pos, boolean value) {
-//        POSITIVE.put(pos, value);
-//    }
+        return world.getBlockState(pos);
+    }
+
+    public static void setPositive(BlockPos pos, boolean value) {
+        POSITIVE.put(pos, value);
+    }
 
 }
