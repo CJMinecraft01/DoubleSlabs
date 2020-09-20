1c00d621054b8a79a6d5e64468cc3f98d091ba5c
CJMinecraft01 <speed2pc2@gmail.com>
2020-09-20 13:39:32 +0100

    Fix #129 and #131

--- src/main/java/cjminecraft/doubleslabs/common/blocks/RaisedCampfireBlock.java
+++ src/main/java/cjminecraft/doubleslabs/common/blocks/RaisedCampfireBlock.java
@@ -1,14 +1,14 @@
 package cjminecraft.doubleslabs.common.blocks;
 
 import cjminecraft.doubleslabs.common.tileentity.RaisedCampfireTileEntity;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CampfireBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
@@ -29,5 +29,10 @@ public class RaisedCampfireBlock extends CampfireBlock {
     @Override
     public TileEntity createNewTileEntity(IBlockReader world) {
         return new RaisedCampfireTileEntity();
+    }
+
+    @Override
+    public ItemStack getPickBlock(BlockState state, RayTraceResult target, IBlockReader world, BlockPos pos, PlayerEntity player) {
+        return new ItemStack(Blocks.CAMPFIRE);
     }
 }
