6bddf635ef6e0aae5a543c5f1a527fbfd5b5dbd4
CJMinecraft01 <CJMinecraft01@users.noreply.github.com>
2022-06-19 12:27:49 +0100

    Remove boiler plate code

--- Fabric/src/main/java/cjminecraft/doubleslabs/fabric/DoubleSlabs.java
+++ Fabric/src/main/java/cjminecraft/doubleslabs/fabric/DoubleSlabs.java
@@ -1,6 +1,5 @@
 package cjminecraft.doubleslabs.fabric;
 
-import cjminecraft.doubleslabs.common.CommonClass;
 import cjminecraft.doubleslabs.common.Constants;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
@@ -9,17 +8,5 @@ public class DoubleSlabs implements ModInitializer {
     
     @Override
     public void onInitialize() {
-        
-        // This method is invoked by the Fabric mod loader when it is ready
-        // to load your mod. You can access Fabric and Common code in this
-        // project.
-
-        // Use Fabric to bootstrap the Common mod.
-        Constants.LOG.info("Hello Fabric world!");
-        CommonClass.init();
-        
-        // Some code like events require special initialization from the
-        // loader specific code.
-        ItemTooltipCallback.EVENT.register(CommonClass::onItemTooltip);
     }
 }
