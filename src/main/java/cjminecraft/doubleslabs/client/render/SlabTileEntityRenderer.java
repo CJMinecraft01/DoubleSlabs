13a0ca7787a9ce0b7e007ba6963b2792bb8b45c8
CJMinecraft01 <CJMinecraft01@users.noreply.github.com>
2022-07-10 19:13:41 +0100

    Remove unused imports

--- src/main/java/cjminecraft/doubleslabs/client/render/SlabTileEntityRenderer.java
+++ src/main/java/cjminecraft/doubleslabs/client/render/SlabTileEntityRenderer.java
@@ -10,12 +10,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.client.renderer.chunk.RenderChunkRegion;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.MinecraftForgeClient;
 
 public class SlabTileEntityRenderer implements BlockEntityRenderer<SlabTileEntity> {
 
