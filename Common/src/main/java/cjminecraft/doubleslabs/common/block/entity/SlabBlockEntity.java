67d702f5462c6aec5fbfb00b9b7113d1ec44e220
CJMinecraft01 <CJMinecraft01@users.noreply.github.com>
2022-07-14 13:04:23 +0100

    Add SlabBlockEntity

---
+++ Common/src/main/java/cjminecraft/doubleslabs/common/block/entity/SlabBlockEntity.java
@@ -0,0 +1,17 @@
+package cjminecraft.doubleslabs.common.block.entity;
+
+import cjminecraft.doubleslabs.common.init.DSBlockEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public abstract class SlabBlockEntity extends BlockEntity {
+    public SlabBlockEntity(BlockPos pos, BlockState state) {
+        super(DSBlockEntities.DYNAMIC_SLAB.get(), pos, state);
+    }
+
+    void markDirtyClient() {
+
+    }
+
+}
