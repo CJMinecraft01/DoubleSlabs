40f9a4fd142c59fc294ecac95e1976ab20304c07
CJMinecraft01 <CJMinecraft01@users.noreply.github.com>
2022-08-19 17:36:40 +0100

    Add `crack` helper

--- Common/src/main/java/cjminecraft/doubleslabs/common/block/DynamicSlabBlock.java
+++ Common/src/main/java/cjminecraft/doubleslabs/common/block/DynamicSlabBlock.java
@@ -3,10 +3,17 @@ package cjminecraft.doubleslabs.common.block;
 import cjminecraft.doubleslabs.api.IBlockInfo;
 import cjminecraft.doubleslabs.common.block.entity.SlabBlockEntity;
 import cjminecraft.doubleslabs.platform.Services;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -14,6 +21,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,6 +40,51 @@ public class DynamicSlabBlock extends BaseEntityBlock {
     public DynamicSlabBlock() {
         super(Properties.of(Material.STONE).noCollission());
         this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public boolean crack(ClientLevel level, BlockState actualState, BlockState state, BlockPos pos, Direction direction, ParticleEngine manager) {
+        if (state.getRenderShape() != RenderShape.INVISIBLE) {
+            int i = pos.getX();
+            int j = pos.getY();
+            int k = pos.getZ();
+            float f = 0.1F;
+            AABB aabb = actualState.getShape(level, pos).bounds();
+            double d0 = (double)i + level.random.nextDouble() * (aabb.maxX - aabb.minX - (double)0.2F) + (double)0.1F + aabb.minX;
+            double d1 = (double)j + level.random.nextDouble() * (aabb.maxY - aabb.minY - (double)0.2F) + (double)0.1F + aabb.minY;
+            double d2 = (double)k + level.random.nextDouble() * (aabb.maxZ - aabb.minZ - (double)0.2F) + (double)0.1F + aabb.minZ;
+            if (direction == Direction.DOWN) {
+                d1 = (double)j + aabb.minY - (double)0.1F;
+            }
+
+            if (direction == Direction.UP) {
+                d1 = (double)j + aabb.maxY + (double)0.1F;
+            }
+
+            if (direction == Direction.NORTH) {
+                d2 = (double)k + aabb.minZ - (double)0.1F;
+            }
+
+            if (direction == Direction.SOUTH) {
+                d2 = (double)k + aabb.maxZ + (double)0.1F;
+            }
+
+            if (direction == Direction.WEST) {
+                d0 = (double)i + aabb.minX - (double)0.1F;
+            }
+
+            if (direction == Direction.EAST) {
+                d0 = (double)i + aabb.maxX + (double)0.1F;
+            }
+
+            Particle particle = manager.createParticle(new BlockParticleOption(ParticleTypes.BLOCK, state), d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            if (particle != null) {
+                particle = particle.setPower(0.2F).scale(0.6F);
+                manager.add(particle);
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Optional<SlabBlockEntity<?>> getTile(BlockGetter world, BlockPos pos) {
