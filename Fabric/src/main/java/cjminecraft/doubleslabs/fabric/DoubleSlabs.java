47bebe43f8bb66ea94ae1c41ca449cd3126e2449
CJMinecraft01 <CJMinecraft01@users.noreply.github.com>
2022-08-22 20:29:42 +0100

    Fabric config beginnings

--- Fabric/src/main/java/cjminecraft/doubleslabs/fabric/DoubleSlabs.java
+++ Fabric/src/main/java/cjminecraft/doubleslabs/fabric/DoubleSlabs.java
@@ -1,12 +1,12 @@
 package cjminecraft.doubleslabs.fabric;
 
-import cjminecraft.doubleslabs.common.Constants;
+import cjminecraft.doubleslabs.fabric.common.config.DSFabricConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
 public class DoubleSlabs implements ModInitializer {
     
     @Override
     public void onInitialize() {
+        DSFabricConfig.loadConfigs();
     }
 }
