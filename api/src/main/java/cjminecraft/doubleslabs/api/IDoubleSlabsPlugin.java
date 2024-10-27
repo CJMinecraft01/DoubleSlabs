cbb9168ed1bab3dc8e1a5f338c511e7c81dd6269
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-27 17:23:08 +0000

    Require plugins to have a unique id

--- api/src/main/java/cjminecraft/doubleslabs/api/IDoubleSlabsPlugin.java
+++ api/src/main/java/cjminecraft/doubleslabs/api/IDoubleSlabsPlugin.java
@@ -1,8 +1,11 @@
 package cjminecraft.doubleslabs.api;
 
 import cjminecraft.doubleslabs.api.registration.ISlabHelperRegistration;
+import net.minecraft.resources.ResourceLocation;
 
 public interface IDoubleSlabsPlugin {
+
+    ResourceLocation getPluginId();
 
     void registerSlabHelpers(ISlabHelperRegistration registration);
 
--- common/src/main/java/cjminecraft/doubleslabs/library/plugins/vanilla/VanillaPlugin.java
+++ common/src/main/java/cjminecraft/doubleslabs/library/plugins/vanilla/VanillaPlugin.java
@@ -3,10 +3,17 @@ package cjminecraft.doubleslabs.library.plugins.vanilla;
 import cjminecraft.doubleslabs.api.DoubleSlabsPlugin;
 import cjminecraft.doubleslabs.api.IDoubleSlabsPlugin;
 import cjminecraft.doubleslabs.api.registration.ISlabHelperRegistration;
+import cjminecraft.doubleslabs.common.Constants;
 import cjminecraft.doubleslabs.library.plugins.vanilla.helpers.MinecraftSlabHelper;
+import net.minecraft.resources.ResourceLocation;
 
 @DoubleSlabsPlugin
 public class VanillaPlugin implements IDoubleSlabsPlugin {
+    @Override
+    public ResourceLocation getPluginId() {
+        return ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "minecraft");
+    }
+
     @Override
     public void registerSlabHelpers(ISlabHelperRegistration registration) {
         registration.addHorizontalHelper(new MinecraftSlabHelper());
