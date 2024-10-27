35705fd719e8b060e5d1fde3991368a6e033c84c
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-27 17:24:25 +0000

    Add an initialisation function

--- common/src/main/java/cjminecraft/doubleslabs/common/Internal.java
+++ common/src/main/java/cjminecraft/doubleslabs/common/Internal.java
@@ -1,21 +1,32 @@
 package cjminecraft.doubleslabs.common;
 
+import cjminecraft.doubleslabs.api.IDoubleSlabsPlugin;
 import cjminecraft.doubleslabs.api.helpers.ISlabHelper;
+import cjminecraft.doubleslabs.common.platform.Services;
+import cjminecraft.doubleslabs.library.load.PluginLoader;
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class Internal {
 
     @Nullable
     private static ISlabHelper slabHelper;
 
-    public static void setSlabHelper(@Nullable ISlabHelper slabHelper) {
-        Internal.slabHelper = slabHelper;
-    }
-
     public static ISlabHelper getSlabHelper() {
         Preconditions.checkState(slabHelper != null, "SlabHelper not initialized");
         return slabHelper;
+    }
+
+    public static void initialise() {
+        List<IDoubleSlabsPlugin> plugins = Services.PLATFORM.getPluginHelper().getPlugins();
+
+        Constants.LOG.info("Detected {} plugins: {}",
+                plugins.size(),
+                Arrays.toString(plugins.stream().map(IDoubleSlabsPlugin::getPluginId).toArray()));
+
+        slabHelper = PluginLoader.registerSlabHelpers(plugins);
     }
 
 }
