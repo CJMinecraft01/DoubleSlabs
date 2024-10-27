20651b2b015c8f1977f27ab5b957f20a1a529bd5
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-27 10:37:58 +0000

    Add an interface which defines the place we register horizontal slab helpers

---
+++ api/src/main/java/cjminecraft/doubleslabs/api/registration/ISlabHelperRegistration.java
@@ -0,0 +1,9 @@
+package cjminecraft.doubleslabs.api.registration;
+
+import cjminecraft.doubleslabs.api.helpers.IHorizontalSlabHelper;
+
+public interface ISlabHelperRegistration {
+
+    void addHorizontalHelper(IHorizontalSlabHelper helper);
+
+}
