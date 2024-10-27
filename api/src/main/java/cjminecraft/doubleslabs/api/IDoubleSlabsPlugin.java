7ccd96a35602d075e4653105b1aaae6718f65b1b
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-27 10:38:51 +0000

    Add an interface which defines the behaviour that all double slabs plugins should implement

---
+++ api/src/main/java/cjminecraft/doubleslabs/api/IDoubleSlabsPlugin.java
@@ -0,0 +1,9 @@
+package cjminecraft.doubleslabs.api;
+
+import cjminecraft.doubleslabs.api.registration.ISlabHelperRegistration;
+
+public interface IDoubleSlabsPlugin {
+
+    void registerSlabHelpers(ISlabHelperRegistration registration);
+
+}
