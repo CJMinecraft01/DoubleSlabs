74dd160799cf17aa39934692c35fa13f8b142943
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-30 21:52:56 +0000

    Add an init class which can register all the blocks and block entities

---
+++ common/src/main/java/cjminecraft/doubleslabs/common/init/DSInit.java
@@ -0,0 +1,10 @@
+package cjminecraft.doubleslabs.common.init;
+
+public class DSInit {
+
+    public static void loadClasses() {
+        DSBlocks.loadClass();
+        DSBlockEntities.loadClass();
+    }
+
+}
