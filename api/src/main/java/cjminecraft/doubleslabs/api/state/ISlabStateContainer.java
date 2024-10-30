4478ddbc3af04583e386e7edd1c209400e30d3d3
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-30 20:54:52 +0000

    Add an interface to state what information needs to be stored to represent slab state
    This contains utility functions to perform actions on the contained block state and block entity.

---
+++ api/src/main/java/cjminecraft/doubleslabs/api/state/ISlabStateContainer.java
@@ -0,0 +1,50 @@
+package cjminecraft.doubleslabs.api.state;
+
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface ISlabStateContainer {
+
+    BlockState getBlockState();
+
+    void setBlockState(BlockState state);
+
+    default boolean hasBlockState() {
+        return !getBlockState().isAir();
+    }
+
+    default void callOnBlockState(Consumer<BlockState> consumer) {
+        if (hasBlockState()) {
+            consumer.accept(getBlockState());
+        }
+    }
+
+    default <T> T callOnBlockState(Function<BlockState, T> function, Supplier<T> orElse) {
+        return hasBlockState() ? function.apply(getBlockState()) : orElse.get();
+    }
+
+    @Nullable
+    BlockEntity getBlockEntity();
+
+    void setBlockEntity(@Nullable BlockEntity blockEntity);
+
+    default boolean hasBlockEntity() {
+        return getBlockEntity() != null;
+    }
+
+    default void callOnBlockEntity(Consumer<BlockEntity> consumer) {
+        if (hasBlockEntity()) {
+            consumer.accept(getBlockEntity());
+        }
+    }
+
+    default <T> T callOnBlockEntity(Function<BlockEntity, T> function, Supplier<T> orElse) {
+        return hasBlockEntity() ? function.apply(getBlockEntity()) : orElse.get();
+    }
+
+}
