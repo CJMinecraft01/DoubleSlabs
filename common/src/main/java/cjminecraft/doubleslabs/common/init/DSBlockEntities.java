dca0493277a62b12588aaf59bf0959d8098ca633
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-30 21:52:35 +0000

    Add a block entity registration class

---
+++ common/src/main/java/cjminecraft/doubleslabs/common/init/DSBlockEntities.java
@@ -0,0 +1,22 @@
+package cjminecraft.doubleslabs.common.init;
+
+import cjminecraft.doubleslabs.api.state.ISlabStateContainer;
+import cjminecraft.doubleslabs.common.Constants;
+import cjminecraft.doubleslabs.common.block.entity.DynamicSlabBlockEntity;
+import cjminecraft.doubleslabs.common.platform.Services;
+import cjminecraft.doubleslabs.registration.RegistrationProvider;
+import cjminecraft.doubleslabs.registration.RegistryObject;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public class DSBlockEntities {
+
+    private static final RegistrationProvider<BlockEntityType<?>> PROVIDER = RegistrationProvider.get(Registries.BLOCK_ENTITY_TYPE, Constants.MOD_ID);
+
+    public static final RegistryObject<BlockEntityType<?>, BlockEntityType<? extends DynamicSlabBlockEntity<? extends ISlabStateContainer>>> DYNAMIC_SLAB = PROVIDER.register("dynamic_slab", () -> BlockEntityType.Builder.of(Services.PLATFORM.getBlockEntityFactory()::createDynamicSlabBlockEntity, DSBlocks.DOUBLE_SLAB.get()).build(null));
+
+    public static void loadClass() {
+
+    }
+
+}
