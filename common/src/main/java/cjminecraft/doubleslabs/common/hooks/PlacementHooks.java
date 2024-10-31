25081382ea242850ee82e8b92cc3dd662cf5aa5b
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-10-31 21:26:27 +0000

    Add a common hook for when an item is used on a block which will try to create a dynamic double slab

---
+++ common/src/main/java/cjminecraft/doubleslabs/common/hooks/PlacementHooks.java
@@ -0,0 +1,145 @@
+package cjminecraft.doubleslabs.common.hooks;
+
+import cjminecraft.doubleslabs.api.helpers.IHorizontalSlabHelper;
+import cjminecraft.doubleslabs.api.helpers.ISlabHelper;
+import cjminecraft.doubleslabs.common.Internal;
+import cjminecraft.doubleslabs.common.block.entity.DynamicSlabBlockEntity;
+import cjminecraft.doubleslabs.common.init.DSBlockEntities;
+import cjminecraft.doubleslabs.common.init.DSBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.phys.BlockHitResult;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+public class PlacementHooks {
+
+    public static Optional<ItemInteractionResult> useItemOnBlock(UseOnContext context) {
+        @Nullable Player player = context.getPlayer();
+        if (player == null) {
+            return Optional.empty();
+        }
+
+        ItemStack itemInHand = context.getItemInHand();
+        if (itemInHand.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Level level = context.getLevel();
+        BlockPos clickedPos = context.getClickedPos();
+        Direction clickedFace = context.getClickedFace();
+        BlockState clickedState = level.getBlockState(clickedPos);
+
+        InteractionHand hand = context.getHand();
+
+        BlockHitResult blockHitResult = context.getHitResult();
+
+        return useItemOnBlock(level, clickedState, clickedPos, clickedFace, player, itemInHand, hand, blockHitResult);
+    }
+
+    private static Optional<ItemInteractionResult> useItemOnBlock(final Level level,
+                                                                  final BlockState clickedBlockState,
+                                                                  final BlockPos clickedPos,
+                                                                  final Direction clickedFace, final Player player,
+                                                                  final ItemStack itemInHand,
+                                                                  final InteractionHand hand,
+                                                                  final BlockHitResult blockHitResult) {
+        final ISlabHelper slabHelper = Internal.getSlabHelper();
+
+        Optional<IHorizontalSlabHelper> optionalItemInHandSlabHelper = slabHelper.getHorizontalSlabHelper(itemInHand);
+
+        // If the held item is a horizontal slab
+        if (optionalItemInHandSlabHelper.isPresent()) {
+            final IHorizontalSlabHelper itemInHandSlabHelper = optionalItemInHandSlabHelper.get();
+
+            // There are two possible states in which placing a horizontal slab will create a dynamic double slab
+
+            final Optional<IHorizontalSlabHelper> optionalClickedBlockSlabHelper =
+                    slabHelper.getHorizontalSlabHelper(clickedBlockState);
+            // 1. If we click on the UP or DOWN face of a slab (so the slab would be inside the same block)
+            if (clickedFace.getAxis().isVertical() && optionalClickedBlockSlabHelper.isPresent()) {
+                final IHorizontalSlabHelper clickedBlockSlabHelper = optionalClickedBlockSlabHelper.get();
+
+                Half half = clickedBlockSlabHelper.getHalf(level, clickedPos, clickedBlockState);
+                // Check that the side clicked is the side that would place the slab within the same block
+                if ((half == Half.BOTTOM && clickedFace == Direction.UP) || (half == Half.TOP && clickedFace == Direction.DOWN)) {
+                    return tryCombineHorizontalSlabs(level, clickedBlockState, clickedPos, player, itemInHand, hand,
+                            blockHitResult, clickedBlockSlabHelper, itemInHandSlabHelper);
+                }
+            }
+
+            final BlockPos posRelativeToClickedFace = clickedPos.relative(clickedFace);
+            final BlockState stateRelativeToClickedFace = level.getBlockState(posRelativeToClickedFace);
+            final Optional<IHorizontalSlabHelper> optionalSlabHelperRelativeToClickedFace =
+                    slabHelper.getHorizontalSlabHelper(stateRelativeToClickedFace);
+            // 2. If we click on the side of a block where there is a horizontal slab opposite the face
+            if (optionalSlabHelperRelativeToClickedFace.isPresent()) {
+                return tryCombineHorizontalSlabs(level, stateRelativeToClickedFace, posRelativeToClickedFace, player,
+                        itemInHand, hand, blockHitResult, optionalSlabHelperRelativeToClickedFace.get(),
+                        itemInHandSlabHelper);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static Optional<ItemInteractionResult> tryCombineHorizontalSlabs(final Level level,
+                                                                             final BlockState slabBlockState,
+                                                                             final BlockPos slabPos,
+                                                                             final Player player,
+                                                                             final ItemStack itemInHand,
+                                                                             final InteractionHand hand,
+                                                                             final BlockHitResult blockHitResult,
+                                                                             final IHorizontalSlabHelper slabBlockHelper, final IHorizontalSlabHelper slabItemHelper) {
+        final ISlabHelper slabHelper = Internal.getSlabHelper();
+
+        // If the slab item and slab block are the same type of slab then use the default behaviour
+        if (slabHelper.areSameTypeOfSlab(slabBlockState, slabBlockHelper, itemInHand, slabItemHelper)) {
+            return Optional.empty();
+        }
+
+        BlockPlaceContext blockPlaceContext = new BlockPlaceContext(player, hand, itemInHand, blockHitResult);
+        @Nullable BlockState stateFromSlabItem = slabItemHelper.getStateFromStack(itemInHand, blockPlaceContext);
+
+        if (stateFromSlabItem == null) {
+            return Optional.empty();
+        }
+
+        Half slabBlockHalf = slabBlockHelper.getHalf(level, slabPos, slabBlockState);
+        Half slabToPlaceHalf = slabBlockHalf == Half.TOP ? Half.BOTTOM : Half.TOP;
+
+        BlockState slabToPlaceState = slabItemHelper.getStateForHalf(level, slabPos, stateFromSlabItem,
+                slabToPlaceHalf);
+
+        @Nullable BlockEntity existingBlockEntity = level.getBlockEntity(slabPos);
+
+        BlockState dynamicDoubleSlabState = DSBlocks.DOUBLE_SLAB.get().defaultBlockState();
+
+        if (!level.setBlock(slabPos, dynamicDoubleSlabState, 3)) {
+            return Optional.empty();
+        }
+
+        Optional<? extends DynamicSlabBlockEntity<?>> optionalDynamicSlabBlockEntity = level.getBlockEntity(slabPos,
+                DSBlockEntities.DYNAMIC_SLAB.get());
+
+        return optionalDynamicSlabBlockEntity.map(dynamicSlabBlockEntity -> {
+            dynamicSlabBlockEntity.setBlockState(slabBlockHalf, slabBlockState);
+            dynamicSlabBlockEntity.setBlockEntity(slabBlockHalf, existingBlockEntity);
+            dynamicSlabBlockEntity.setBlockState(slabToPlaceHalf, slabToPlaceState);
+
+            return ItemInteractionResult.SUCCESS;
+        });
+    }
+
+}
