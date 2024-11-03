4023c855aed1c7dcfe10ff9f15100d71017b277e
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-11-03 16:42:56 +0000

    Set the render shape to be a block model

--- common/src/main/java/cjminecraft/doubleslabs/common/block/DynamicSlabBlock.java
+++ common/src/main/java/cjminecraft/doubleslabs/common/block/DynamicSlabBlock.java
@@ -4,6 +4,7 @@ import cjminecraft.doubleslabs.common.platform.Services;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -27,5 +28,10 @@ public class DynamicSlabBlock extends BaseEntityBlock {
     @Override
     protected MapCodec<? extends BaseEntityBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 }
