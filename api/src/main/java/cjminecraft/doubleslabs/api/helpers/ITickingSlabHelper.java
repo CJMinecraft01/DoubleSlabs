9436690dac27f9ba96e5adb19f4bc64af7f00955
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2024-11-27 21:01:52 +0000

    Add a new type of helper specifically for handling ticking blocks

---
+++ api/src/main/java/cjminecraft/doubleslabs/api/helpers/ITickingSlabHelper.java
@@ -0,0 +1,15 @@
+package cjminecraft.doubleslabs.api.helpers;
+
+import cjminecraft.doubleslabs.api.state.ISlabStateContainer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+
+public interface ITickingSlabHelper {
+
+    boolean isRandomlyTicking(BlockState state);
+
+    void randomTick(ISlabStateContainer stateContainer, ServerLevel level, BlockPos pos, RandomSource random);
+
+}
