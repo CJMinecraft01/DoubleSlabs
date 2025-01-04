fa8f957916e98349dc9732f60a636acfd3cfe149
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-01-04 17:16:56 +0000

    Fix forge boot-up

--- forge/src/main/java/cjminecraft/doubleslabs/forge/common/DoubleSlabs.java
+++ forge/src/main/java/cjminecraft/doubleslabs/forge/common/DoubleSlabs.java
@@ -8,15 +8,14 @@ import cjminecraft.doubleslabs.forge.common.init.DSForgeBlocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class DoubleSlabs {
 
-    public DoubleSlabs(ModLoadingContext context) {
-        IEventBus modBus = context.<FMLJavaModLoadingContext>extension().getModEventBus();
+    public DoubleSlabs(FMLJavaModLoadingContext context) {
+        IEventBus modBus = context.getModEventBus();
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> DoubleSlabsClient.addListeners(modBus));
 
