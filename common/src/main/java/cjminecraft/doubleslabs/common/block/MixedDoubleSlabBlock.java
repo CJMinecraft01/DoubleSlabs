0e96b2420300c47b2e5493ff8496d885e2e8ef7d
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-04-22 20:05:02 +0100

    Remove unused imports

--- common/src/main/java/cjminecraft/doubleslabs/common/block/MixedDoubleSlabBlock.java
+++ common/src/main/java/cjminecraft/doubleslabs/common/block/MixedDoubleSlabBlock.java
@@ -9,8 +9,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class MixedDoubleSlabBlock extends DynamicSlabBlock {
