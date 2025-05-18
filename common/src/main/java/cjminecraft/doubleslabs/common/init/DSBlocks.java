d8d01c68efc9bc4bea4a7e3f246f7b8d03012273
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-05-18 17:36:46 +0100

    Add a tag for all mixed slab variants

--- common/src/main/java/cjminecraft/doubleslabs/common/init/DSBlocks.java
+++ common/src/main/java/cjminecraft/doubleslabs/common/init/DSBlocks.java
@@ -4,6 +4,8 @@ import cjminecraft.doubleslabs.common.block.MixedDoubleSlabBlock;
 import cjminecraft.doubleslabs.common.platform.Services;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Supplier;
@@ -21,5 +23,7 @@ public class DSBlocks {
     // We have to wrap the supplier to ensure class loading gets the mixed slab supplier
     public static final Supplier<MixedDoubleSlabBlock> MIXED_SLAB = () -> Services.PLATFORM.getBlocks().getMixedSlabBlock();
     public static final Supplier<MixedDoubleSlabBlock> TRANSPARENT_MIXED_SLAB = () -> Services.PLATFORM.getBlocks().getTransparentMixedSlabBlock();
+
+    public static final TagKey<Block> MIXED_SLABS = TagKey.create(Registries.BLOCK, id("mixed_slabs"));
 
 }
