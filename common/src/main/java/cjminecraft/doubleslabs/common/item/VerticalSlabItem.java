17b2d89592cfc4b70671ee7e8e5261c7cfdb9ba8
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-05-25 13:28:54 +0100

    Add a vertical slab item

---
+++ common/src/main/java/cjminecraft/doubleslabs/common/item/VerticalSlabItem.java
@@ -0,0 +1,10 @@
+package cjminecraft.doubleslabs.common.item;
+
+import cjminecraft.doubleslabs.common.init.DSBlocks;
+import net.minecraft.world.item.BlockItem;
+
+public class VerticalSlabItem extends BlockItem {
+    public VerticalSlabItem() {
+        super(DSBlocks.VERTICAL_SLAB.get(), new Properties());
+    }
+}
