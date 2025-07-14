aa6ca6e4014a0fa904683329caf1ed32f4d0106c
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 20:25:33 +0100

    Bake the vertical slab and initialise the client internals

--- forge/src/main/java/cjminecraft/doubleslabs/forge/client/DoubleSlabsClient.java
+++ forge/src/main/java/cjminecraft/doubleslabs/forge/client/DoubleSlabsClient.java
@@ -1,7 +1,9 @@
 package cjminecraft.doubleslabs.forge.client;
 
+import cjminecraft.doubleslabs.client.ClientInternal;
 import cjminecraft.doubleslabs.client.hooks.DynamicSlabBlockClientHooks;
 import cjminecraft.doubleslabs.client.model.VerticalSlabItemBakedModel;
+import cjminecraft.doubleslabs.client.model.VerticalSlabModelBaker;
 import cjminecraft.doubleslabs.forge.client.model.ForgeModelBaker;
 import cjminecraft.doubleslabs.forge.client.model.MixedDoubleSlabBakedModel;
 import cjminecraft.doubleslabs.forge.common.init.DSForgeBlocks;
@@ -10,6 +12,7 @@ import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import static cjminecraft.doubleslabs.common.init.DSBlocks.*;
 
@@ -22,6 +25,7 @@ public class DoubleSlabsClient {
     public static void addListeners(IEventBus mod) {
         mod.addListener(DoubleSlabsClient::replaceModels);
         mod.addListener(DoubleSlabsClient::registerBlockColours);
+        mod.addListener(DoubleSlabsClient::bakeModels);
     }
 
     private static void replaceModels(final ModelEvent.ModifyBakingResult event) {
@@ -30,6 +34,16 @@ public class DoubleSlabsClient {
 
         final var forgeModelBaker = new ForgeModelBaker(event.getModelBakery(), Minecraft.getInstance().getModelManager());
         event.getModels().put(VERTICAL_SLAB_ITEM_MODEL, new VerticalSlabItemBakedModel(forgeModelBaker));
+    }
+
+    private static void bakeModels(final ModelEvent.BakingCompleted event) {
+        final var forgeModelBaker = new ForgeModelBaker(event.getModelBakery(), event.getModelManager());
+        final var verticalSlabModelBaker = new VerticalSlabModelBaker(forgeModelBaker);
+
+        verticalSlabModelBaker.bakeBlocks(ForgeRegistries.BLOCKS);
+        verticalSlabModelBaker.bakeItems(ForgeRegistries.ITEMS, ForgeRegistries.ITEMS::getKey);
+
+        ClientInternal.initialise(verticalSlabModelBaker.createModelHelper());
     }
 
     private static void registerBlockColours(final RegisterColorHandlersEvent.Block event) {
