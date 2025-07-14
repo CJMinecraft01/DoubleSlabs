d7a97aee5311f683fa348d82623d323d58b33e45
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 16:35:43 +0100

    Create content that stores the vertical slab for a vertical slab item

---
+++ common/src/main/java/cjminecraft/doubleslabs/common/item/component/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package cjminecraft.doubleslabs.common.item.component;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
