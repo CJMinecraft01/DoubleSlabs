e4b84cca35d61e63b443ab8eaf8f9b7bf2cc6d3f
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 20:14:23 +0100

    Store the vertical slab model helper in `ClientInternal`

--- common/src/main/java/cjminecraft/doubleslabs/client/ClientInternal.java
+++ common/src/main/java/cjminecraft/doubleslabs/client/ClientInternal.java
@@ -1,7 +1,11 @@
 package cjminecraft.doubleslabs.client;
 
+import cjminecraft.doubleslabs.library.helpers.VerticalSlabModelHelper;
+import com.google.common.base.Preconditions;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.BakedModel;
+
+import javax.annotation.Nullable;
 
 public class ClientInternal {
 
@@ -9,6 +13,18 @@ public class ClientInternal {
 
     public static BakedModel getFallbackModel() {
         return Minecraft.getInstance().getModelManager().getMissingModel();
+    }
+
+    @Nullable
+    private static VerticalSlabModelHelper verticalSlabModelHelper;
+
+    public static VerticalSlabModelHelper getVerticalSlabModelHelper() {
+        Preconditions.checkNotNull(verticalSlabModelHelper, "VerticalSlabModelHelper not initialized");
+        return verticalSlabModelHelper;
+    }
+
+    public static void initialise(VerticalSlabModelHelper verticalSlabModelHelper) {
+        ClientInternal.verticalSlabModelHelper = verticalSlabModelHelper;
     }
 
 }
