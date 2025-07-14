d179c7b40c242bb56d849d1b8e8c63da3766d166
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 20:49:13 +0100

    Replace the vertical slab item model

--- fabric/src/main/java/cjminecraft/doubleslabs/fabric/client/DoubleSlabsClient.java
+++ fabric/src/main/java/cjminecraft/doubleslabs/fabric/client/DoubleSlabsClient.java
@@ -1,17 +1,18 @@
 package cjminecraft.doubleslabs.fabric.client;
 
+import cjminecraft.doubleslabs.client.model.VerticalSlabItemBakedModel;
 import cjminecraft.doubleslabs.fabric.client.model.MixedDoubleSlabBakedModel;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 
-import static cjminecraft.doubleslabs.common.init.DSBlocks.MIXED_SLAB_ID;
-import static cjminecraft.doubleslabs.common.init.DSBlocks.TRANSPARENT_MIXED_SLAB_ID;
+import static cjminecraft.doubleslabs.common.init.DSBlocks.*;
 
 public class DoubleSlabsClient implements ClientModInitializer {
 
     private static final ModelResourceLocation MIXED_SLAB_MODEL = new ModelResourceLocation(MIXED_SLAB_ID, "");
     private static final ModelResourceLocation TRANSPARENT_MIXED_SLAB_MODEL = new ModelResourceLocation(TRANSPARENT_MIXED_SLAB_ID, "");
+    private static final ModelResourceLocation VERTICAL_SLAB_ITEM_MODEL = new ModelResourceLocation(VERTICAL_SLAB_ID, "inventory");
 
     @Override
     public void onInitializeClient() {
@@ -22,11 +23,19 @@ public class DoubleSlabsClient implements ClientModInitializer {
         pluginContext.modifyModelAfterBake().register((original, context) -> {
             final var location = context.topLevelId();
 
-            if (location != null && (location.equals(MIXED_SLAB_MODEL) || location.equals(TRANSPARENT_MIXED_SLAB_MODEL))) {
-                return new MixedDoubleSlabBakedModel();
-            } else {
+            if (location == null) {
                 return original;
             }
+
+            if (location.equals(MIXED_SLAB_MODEL) || location.equals(TRANSPARENT_MIXED_SLAB_MODEL)) {
+                return new MixedDoubleSlabBakedModel();
+            }
+
+            if (location.equals(VERTICAL_SLAB_ITEM_MODEL)) {
+                return new VerticalSlabItemBakedModel(context.baker());
+            }
+
+            return original;
         });
     }
 
