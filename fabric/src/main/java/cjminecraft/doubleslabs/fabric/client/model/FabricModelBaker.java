f950ab59d9b3a8324ea2dd1bbaf655df3ad5a484
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 21:22:09 +0100

    Add a fabric implementation for model baking

---
+++ fabric/src/main/java/cjminecraft/doubleslabs/fabric/client/model/FabricModelBaker.java
@@ -0,0 +1,11 @@
+package cjminecraft.doubleslabs.fabric.client.model;
+
+import cjminecraft.doubleslabs.client.model.PlatformIndependentModelBaker;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelManager;
+
+public class FabricModelBaker extends PlatformIndependentModelBaker {
+    public FabricModelBaker(ModelBakery bakery, ModelManager manager) {
+        super(bakery, manager);
+    }
+}
