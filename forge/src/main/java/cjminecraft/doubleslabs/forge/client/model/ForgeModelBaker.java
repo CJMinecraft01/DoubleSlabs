2e99cb9894c945822e2efeb2d81029105f5703a4
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 20:22:21 +0100

    Add a forge implementation of the model baker

---
+++ forge/src/main/java/cjminecraft/doubleslabs/forge/client/model/ForgeModelBaker.java
@@ -0,0 +1,28 @@
+package cjminecraft.doubleslabs.forge.client.model;
+
+import cjminecraft.doubleslabs.client.model.PlatformIndependentModelBaker;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.*;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class ForgeModelBaker extends PlatformIndependentModelBaker {
+
+    public ForgeModelBaker(ModelBakery bakery, ModelManager manager) {
+        super(bakery, manager);
+    }
+
+    @Override
+    public @Nullable BakedModel bake(ResourceLocation resourceLocation, ModelState modelState, Function<Material, TextureAtlasSprite> function) {
+        final var model = getModel(resourceLocation);
+
+        return model.bake(this, function, modelState);
+    }
+
+    @Override
+    public Function<Material, TextureAtlasSprite> getModelTextureGetter() {
+        return this::getTextureSprite;
+    }
+}
