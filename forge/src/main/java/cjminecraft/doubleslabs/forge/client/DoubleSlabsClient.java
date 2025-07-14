26165730692dce0ff7e3809c5931e26c9f9b7147
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-14 20:24:13 +0100

    Replace the vertical slab item model with the custom one

--- forge/src/main/java/cjminecraft/doubleslabs/forge/client/DoubleSlabsClient.java
+++ forge/src/main/java/cjminecraft/doubleslabs/forge/client/DoubleSlabsClient.java
@@ -1,29 +1,35 @@
 package cjminecraft.doubleslabs.forge.client;
 
 import cjminecraft.doubleslabs.client.hooks.DynamicSlabBlockClientHooks;
+import cjminecraft.doubleslabs.client.model.VerticalSlabItemBakedModel;
+import cjminecraft.doubleslabs.forge.client.model.ForgeModelBaker;
 import cjminecraft.doubleslabs.forge.client.model.MixedDoubleSlabBakedModel;
 import cjminecraft.doubleslabs.forge.common.init.DSForgeBlocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 
-import static cjminecraft.doubleslabs.common.init.DSBlocks.MIXED_SLAB_ID;
-import static cjminecraft.doubleslabs.common.init.DSBlocks.TRANSPARENT_MIXED_SLAB_ID;
+import static cjminecraft.doubleslabs.common.init.DSBlocks.*;
 
 public class DoubleSlabsClient {
 
     private static final ModelResourceLocation MIXED_SLAB_MODEL = new ModelResourceLocation(MIXED_SLAB_ID, "");
     private static final ModelResourceLocation TRANSPARENT_MIXED_SLAB_MODEL = new ModelResourceLocation(TRANSPARENT_MIXED_SLAB_ID, "");
+    private static final ModelResourceLocation VERTICAL_SLAB_ITEM_MODEL = new ModelResourceLocation(VERTICAL_SLAB_ID, "inventory");
 
     public static void addListeners(IEventBus mod) {
-        mod.addListener(DoubleSlabsClient::bakeModels);
+        mod.addListener(DoubleSlabsClient::replaceModels);
         mod.addListener(DoubleSlabsClient::registerBlockColours);
     }
 
-    private static void bakeModels(final ModelEvent.ModifyBakingResult event) {
+    private static void replaceModels(final ModelEvent.ModifyBakingResult event) {
         event.getModels().put(MIXED_SLAB_MODEL, new MixedDoubleSlabBakedModel());
         event.getModels().put(TRANSPARENT_MIXED_SLAB_MODEL, new MixedDoubleSlabBakedModel());
+
+        final var forgeModelBaker = new ForgeModelBaker(event.getModelBakery(), Minecraft.getInstance().getModelManager());
+        event.getModels().put(VERTICAL_SLAB_ITEM_MODEL, new VerticalSlabItemBakedModel(forgeModelBaker));
     }
 
     private static void registerBlockColours(final RegisterColorHandlersEvent.Block event) {
