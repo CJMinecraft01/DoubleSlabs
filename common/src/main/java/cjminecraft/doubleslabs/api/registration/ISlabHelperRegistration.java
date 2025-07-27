0c0803ac9431170eb469d857b9cf3a544a72b28d
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-07-27 15:47:37 +0100

    Add registration for vertical helpers

--- common/src/main/java/cjminecraft/doubleslabs/api/registration/ISlabHelperRegistration.java
+++ common/src/main/java/cjminecraft/doubleslabs/api/registration/ISlabHelperRegistration.java
@@ -2,11 +2,14 @@ package cjminecraft.doubleslabs.api.registration;
 
 import cjminecraft.doubleslabs.api.helpers.IHorizontalSlabHelper;
 import cjminecraft.doubleslabs.api.helpers.ISlabAbilities;
+import cjminecraft.doubleslabs.api.helpers.IVerticalSlabHelper;
 import net.minecraft.world.level.block.Block;
 
 public interface ISlabHelperRegistration {
 
     void addHorizontalHelper(IHorizontalSlabHelper helper);
+
+    void addVerticalHelper(IVerticalSlabHelper helper);
 
     void registerSlabAbilities(ISlabAbilities abilities, Block... blocks);
 
