034b993ecb457a26756ae403783e35505051bf20
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2026-01-04 14:05:28 +0000

    Don't assume we have a block entity when getting a capability

--- forge/src/main/java/cjminecraft/doubleslabs/forge/common/state/ForgeSlabStateContainer.java
+++ forge/src/main/java/cjminecraft/doubleslabs/forge/common/state/ForgeSlabStateContainer.java
@@ -2,7 +2,6 @@ package cjminecraft.doubleslabs.forge.common.state;
 
 import cjminecraft.doubleslabs.api.state.IDynamicSlabStateContainer;
 import cjminecraft.doubleslabs.library.state.SlabStateContainer;
-import com.google.common.base.Preconditions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraftforge.common.capabilities.Capability;
@@ -19,13 +18,11 @@ public class ForgeSlabStateContainer extends SlabStateContainer implements ICapa
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction) {
-        Preconditions.checkState(blockEntity != null, "Cannot get the capability from the block entity if the block entity does not exist");
-        return blockEntity.getCapability(capability, direction);
+        return blockEntity != null ? blockEntity.getCapability(capability, direction) : LazyOptional.empty();
     }
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability) {
-        Preconditions.checkState(blockEntity != null, "Cannot get the capability from the block entity if the block entity does not exist");
-        return blockEntity.getCapability(capability);
+        return blockEntity != null ?  blockEntity.getCapability(capability) : LazyOptional.empty();
     }
 }
